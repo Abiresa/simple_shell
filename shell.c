dfade42697f04341629cd99f95a4ad5bd0975343
OumaimaSf <oumaimasaaouf@gmail.com>
2023-06-29 23:44:24 +0100

    SIMPLE_Shell

--- shell.c
+++ shell.c
@@ -7,6 +7,7 @@
 #include "shell.h"
 
 #define MAX_INPUT_LENGTH 100
+#define MAX_ARGUMENTS 10
 
 void shell_loop(void) {
     char input[MAX_INPUT_LENGTH];
@@ -23,13 +24,23 @@ void shell_loop(void) {
         if (strcmp(input, "exit") == 0)
             break;
 
+        char *arguments[MAX_ARGUMENTS];
+        char *token = strtok(input, " ");
+        int argc = 0;
+
+        while (token != NULL && argc < MAX_ARGUMENTS - 1) {
+            arguments[argc++] = token;
+            token = strtok(NULL, " ");
+        }
+        arguments[argc] = NULL;
+
         pid = fork();
         if (pid < 0) {
             perror("fork");
             exit(EXIT_FAILURE);
         } else if (pid == 0) {
-            execlp(input, input, (char *)NULL);
-            perror(input);
+            execvp(arguments[0], arguments);
+            perror(arguments[0]);
             exit(EXIT_FAILURE);
         } else {
             waitpid(pid, &status, 0);
